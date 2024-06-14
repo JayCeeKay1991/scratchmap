af2f29a0b308f5d512c08559b52a6539169ee5b0
JayCeeKay1991 <johanna.kveton@gmail.com>
2024-06-14 10:45:38 +0200

    style: marker

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -2,13 +2,28 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import ControlPanel from "./ControlPanel";
 import MapContainerComp from "./MapContainer";
+import L from "leaflet";
 import { TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 import MapEffect from "./MapEffect";
 import { getAllTrips } from "../services/tripService";
 import { Trip } from "../types/types";
 import { Location } from "../types/types";
 import moment from "moment";
-import Login from "./Login";
+
+// Fixing marker icon issue
+const defaultIcon = L.icon({
+  iconRetinaUrl: "/images/marker-icon-2x.png",
+  iconUrl: "/images/marker-icon.png",
+  shadowUrl: "/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+// Apply the default icon to all markers
+L.Marker.prototype.options.icon = defaultIcon;
 
 const App = () => {
   const [selectedLocation, setSelectedLocation] = useState<Location | null>(
